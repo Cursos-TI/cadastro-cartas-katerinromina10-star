5aba2cdb4f435d5ac4a9b343cc8f9ceeb6534310
katerinromina10-star <katerinromina10@gmail.com>
2025-11-17 21:45:40 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,73 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-auhfiewkfjhiun
+
+  // Carta 1
+  char estado1;
+  char codigo1[4];
+  char cidade1[50];
+  int populacao1;
+  float area1;
+  float pib1;
+  int pontos1;
+  
+  //Carta 2
+  char estado2;
+  char codigo2[4];
+  char cidade2[50];
+  int populacao2;
+  float area2;
+  float pib2;
+  int pontos2;
+
   // Área para entrada de dados
+
+  //Entrada carta 1
+
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado1);
+
+    printf("Código da Carta: ");
+    scanf("%s", codigo1);
+
+    printf("Nome da Cidade: ");
+    scanf("%s", cidade1);
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Area (km²): ");
+    scanf("%f", &area1);
+
+    printf("PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Número de Pontos Turisticos: ");
+    scanf("%d", &pontos1);
+
+    // Entrada carta 2
+
+   printf("Estado (A-H): ");
+    scanf(" %c", &estado2);
+
+    printf("Código da Carta: ");
+    scanf("%s", codigo2);
+
+    printf("Nome da Cidade: ");
+    scanf("%s", cidade2);
+
+    printf("População: ");
+    scanf("%d", &populacao2);
+
+    printf("Area (km²): ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Número de Pontos Turisticos: ");
+    scanf("%d", &pontos2);
+
 
   // Área para exibição dos dados da cidade
 
