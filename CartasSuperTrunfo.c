4c7c91a488fbfae3b4264b92ec821be9fb74e39f
katerinromina10-star <katerinromina10@gmail.com>
2025-11-26 15:03:14 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,7 +11,7 @@ int main() {
   char estado1;
   char codigo1[4];
   char cidade1[50];
-  long int populacao1;
+  unsigned long int populacao1;
   float area1;
   float pib1;
   int pontos1;
@@ -20,7 +20,7 @@ int main() {
   char estado2;
   char codigo2[4];
   char cidade2[50];
-  long int populacao2;
+  unsigned long int populacao2;
   float area2;
   float pib2;
   int pontos2;
@@ -39,7 +39,7 @@ int main() {
     scanf("%s", &cidade1);
 
     printf("População: ");
-    scanf("%ld", &populacao1);
+    scanf("%lu", &populacao1);
 
     printf("Area (km²): ");
     scanf("%f", &area1);
@@ -63,7 +63,7 @@ int main() {
     scanf("%s", &cidade2);
 
     printf("População: ");
-    scanf("%ld", &populacao2);
+    scanf("%lu", &populacao2);
 
     printf("Area (km²): ");
     scanf("%f", &area2);
@@ -82,7 +82,7 @@ int main() {
     printf("Estado: %c\n", estado1);
     printf("Codigo: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", cidade1);
-    printf("População: %ld\n", populacao1);
+    printf("População: %lu\n", populacao1);
     printf("Area: %.2f km²\n", area1);
     printf("PIB: %.2f bilhoes de reais\n", pib1);
     printf("Número de Pontos Turisticos: %d\n", pontos1);
@@ -92,7 +92,7 @@ int main() {
     printf("Estado: %c\n", estado2);
     printf("Codigo: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", cidade2);
-    printf("População: %ld\n", populacao2);
+    printf("População: %lu\n", populacao2);
     printf("Area: %.2f km²\n", area2);
     printf("PIB: %.2f bilhoes de reais\n", pib2);
     printf("Número de Pontos Turisticos: %d\n", pontos2);
@@ -130,19 +130,19 @@ printf("Carta 2: %.2f PIB per Capita\n", pibpercapita2);
 
   printf("\nComparação de Cartas\n");
   long int comparacaopopulacao = populacao1 > populacao2;
-  float comparacaoarea = area1 < area2;
+  float comparacaoarea = area1 > area2;
   float comparacaopib = pib1> pib2;
   int comparacaopontos = pontos1 > pontos2;
-  float comparacaopibpercarpita =  pibpercapita1 < pibpercapita2;
-  float comparacaoinversodensidade = inversodensidade1 > inversodensidade2;
+  float comparacaopibpercarpita =  pibpercapita1 > pibpercapita2;
+  float comparacaoinversodensidade = inversodensidade1 < inversodensidade2;
 
   //Exibir os Resultados das Comparações
 
-  printf ("A Populacao da carta 1 venceu: %.1ld\n", comparacaopopulacao);
-  printf ("A Area da carta 2 venceu: %.1f\n", comparacaoarea);
-  printf ("O PIB da carta 1 venceu: %.1f\n", comparacaopib);
-  printf ("Os Pontos turisticos da carta 1 venceram: %.1d\n", comparacaopontos);
-  printf ("O PIB per capita da carta 2 venceu: %.1f\n", comparacaopibpercarpita);
-  printf ("A densidade da carta 1 venceu: %.1f\n", comparacaoinversodensidade);
+  printf ("A População da carta 1 venceu %.lu\n", comparacaopopulacao);
+  printf ("A Área da Carta 1 venceu %f\n", comparacaoarea);
+  printf ("O PIB da Carta 1 venceu %f\n", comparacaopib);
+  printf ("Os Pontos turisticos da carta 1 vencerom: %d\n", comparacaopontos);
+  printf ("O PIB per capita da carta 1 venceu: %f\n", comparacaopibpercarpita);
+  printf ("A densidade da carta 2 venceu: %f\n", comparacaoinversodensidade);
 return 0;
 } 
